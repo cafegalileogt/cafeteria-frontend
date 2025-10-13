2f3ebb54e387977268fdaa160d263f617e82ac31
Jose Melendez <23001327@galileo.edu>
2025-10-13 02:05:47 -0600

    Feat: Redireccionar paginas para Android y Web

---
+++ frontend-app/src/app/admin/login.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function login() {
+  return (
+    <View>
+      <Text>¡Bienvenido a la página de Administrador!</Text>
+    </View>
+  );
+}
