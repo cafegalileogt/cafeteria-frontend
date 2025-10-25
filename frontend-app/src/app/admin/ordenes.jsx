11c7797150582bda00c852efe229b61ad3caa007
Jose Melendez <23001327@galileo.edu>
2025-10-25 00:59:24 -0600

    Feat: Agregar vista tabla de ordenes

--- frontend-app/src/app/admin/ordenes.jsx
+++ frontend-app/src/app/admin/ordenes.jsx
@@ -1,12 +1,25 @@
-import { Text, View } from "react-native";
+import { FlatList, ScrollView, Text, View } from "react-native";
 import Styles from "../../styles/ordenesStyle";
-import { Nunito_900Black, useFonts } from "@expo-google-fonts/nunito";
+import {
+  Nunito_900Black,
+  Nunito_400Regular,
+  useFonts,
+} from "@expo-google-fonts/nunito";
 import { SplashScreen } from "expo-router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState([
+    { idOrden: "1235", nombre: "usuario01", estado: "Entregada" },
+    { idOrden: "1236", nombre: "usuario02", estado: "Entregada" },
+    { idOrden: "1237", nombre: "usuario04", estado: "En preparacion" },
+    { idOrden: "1238", nombre: "usuario05", estado: "Pendiente pago" },
+    { idOrden: "1239", nombre: "usuario03", estado: "En preparacion" },
+  ]);
+
   const [loaded, error] = useFonts({
     Nunito_900Black,
+    Nunito_400Regular,
   });
 
   useEffect(() => {
@@ -19,11 +32,43 @@ export default function Home() {
     return null;
   }
 
+  const renderItem = ({ item, index }) => {
+    return (
+      <View style={Styles.row}>
+        <Text style={[Styles.cell, { width: "20%" }]}>{item.idOrden}</Text>
+        <Text style={[Styles.cell, { width: "40%" }]}>{item.nombre}</Text>
+        <Text style={[Styles.cell, { width: "40%" }]}>{item.estado}</Text>
+      </View>
+    );
+  };
+
   return (
     <View style={Styles.background}>
       <View style={Styles.page}>
         <Text style={Styles.title}>Órdenes</Text>
         <View style={Styles.separator} />
+        <View style={Styles.container}>
+          <ScrollView>
+            <View style={Styles.tabla}>
+              <View style={Styles.header}>
+                <Text style={[Styles.headerText, { width: "20%" }]}>
+                  # Orden
+                </Text>
+                <Text style={[Styles.headerText, { width: "40%" }]}>
+                  Nombre
+                </Text>
+                <Text style={[Styles.headerText, { width: "40%" }]}>
+                  Estado
+                </Text>
+              </View>
+              <FlatList
+                data={data}
+                renderItem={renderItem}
+                keyExtractor={(item, index) => index.toString()}
+              />
+            </View>
+          </ScrollView>
+        </View>
       </View>
     </View>
   );
--- frontend-app/src/styles/ordenesStyle.js
+++ frontend-app/src/styles/ordenesStyle.js
@@ -22,4 +22,35 @@ export default StyleSheet.create({
     borderBottomColor: "#B89A59",
     borderBottomWidth: StyleSheet.hairlineWidth * 5,
   },
+  container: {
+    height: "90%",
+    width: "90%",
+    alignSelf: "center",
+    paddingTop: 25,
+  },
+  header: {
+    flex: 1,
+    flexDirection: "row",
+    paddingVertical: 10,
+    borderWidth: 1,
+    borderColor: "#AAAAAA",
+  },
+  headerText: {
+    fontFamily: "Nunito_400Regular",
+    fontSize: 24,
+    textAlign: "center",
+    alignSelf: "center",
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  cell: {
+    fontFamily: "Nunito_400Regular",
+    fontSize: 24,
+    textAlign: "center",
+    alignSelf: "center",
+    borderWidth: 1,
+    borderColor: "#AAAAAA",
+  },
 });
