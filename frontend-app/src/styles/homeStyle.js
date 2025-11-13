d395a79921073d5ef4a2e36833af78a7d2f310a1
Jose Melendez <23001327@galileo.edu>
2025-11-13 00:28:09 -0600

    Fix: formato de archivo de estilo

--- frontend-app/src/styles/homeStyle.js
+++ frontend-app/src/styles/homeStyle.js
@@ -2,45 +2,45 @@ import { StyleSheet, Dimensions } from "react-native";
 const { width, height } = Dimensions.get("window");
 
 export default StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+	},
 
-  headerContainer: {
-    position: "relative",
-    height: height * 0.15,
-    justifyContent: "center",
-  },
-  headerImage: {
-    width: "100%",
-    height: "100%",
-  },
-  overlay: {
-    ...StyleSheet.absoluteFillObject,
-    backgroundColor: "rgba(255,255,255,0.2)",
-  },
-  welcome: {
-    position: "absolute",
-    left: 20,
-    fontSize: width < 500 ? 24 : 26,
-    fontFamily: "Nunito_500Medium",
-    color: "#000",
-    width:"70%",
-  },
-  logo: {
-    position: "absolute",
-    top: 20,
-    right: 20,
-    width: width * 0.28,
-    height: height * 0.06,
-  },
+	headerContainer: {
+		position: "relative",
+		height: height * 0.15,
+		justifyContent: "center",
+	},
+	headerImage: {
+		width: "100%",
+		height: "100%",
+	},
+	overlay: {
+		...StyleSheet.absoluteFillObject,
+		backgroundColor: "rgba(255,255,255,0.2)",
+	},
+	welcome: {
+		position: "absolute",
+		left: 20,
+		fontSize: width < 500 ? 24 : 26,
+		fontFamily: "Nunito_400Regular",
+		color: "#000",
+		width: "70%",
+	},
+	logo: {
+		position: "absolute",
+		top: 20,
+		right: 20,
+		width: width * 0.28,
+		height: height * 0.06,
+	},
 
-  // Contenido Principal
-  mainSection: {
-    flex: 1,
-    flexDirection: "row",
-  },
+	// Contenido Principal
+	mainSection: {
+		flex: 1,
+		flexDirection: "row",
+	},
 
   // categorías
   sidebar: {
@@ -81,52 +81,51 @@ export default StyleSheet.create({
     backgroundColor: "#fff",
   },
 
-  // Productos
-  menuSection: {
-    flex: 1,
-    padding: width < 500 ? 10 : 15,
-  },
-  sectionTitle: {
-    fontFamily: "Playfair Display",
-    fontWeight: "900",
-    fontSize: width < 500 ? 30 : 38,
-    color: "#000",
-    marginVertical: 15,
-    textAlign: "center",
-  },
-  menuGrid: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-  },
-  card: {
-    width: "30%",
-    alignItems: "center",
-    marginBottom: 25,
-  },
-  placeholderImage: {
-    width: width < 500 ? 65 : 75,
-    height: width < 500 ? 65 : 75,
-    borderRadius: 10,
-    backgroundColor: "#D9D9D9",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  placeholderText: {
-    fontSize: width < 500 ? 18 : 22,
-    fontWeight: "bold",
-    color: "#7b5e2a",
-  },
-  foodName: {
-    fontFamily: "Inter",
-    fontSize: 14,
-    color: "#000",
-    marginTop: 6,
-    textAlign: "center",
-  },
-  foodPrice: {
-    fontFamily: "Inria Sans",
-    fontSize: 12,
-    color: "#B89A59",
-  },
+	// Productos
+	menuSection: {
+		flex: 1,
+		padding: width < 500 ? 10 : 15,
+	},
+	sectionTitle: {
+		fontFamily: "Nunito_700Bold",
+		fontSize: width < 500 ? 30 : 38,
+		color: "#000",
+		marginVertical: 15,
+		textAlign: "center",
+	},
+	menuGrid: {
+		flexDirection: "row",
+		flexWrap: "wrap",
+		justifyContent: "space-around",
+	},
+	card: {
+		width: "30%",
+		alignItems: "center",
+		marginBottom: 25,
+	},
+	placeholderImage: {
+		width: width < 500 ? 65 : 75,
+		height: width < 500 ? 65 : 75,
+		borderRadius: 10,
+		backgroundColor: "#D9D9D9",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	placeholderText: {
+		fontSize: width < 500 ? 18 : 22,
+		fontFamily: "Nunito_700Bold",
+		color: "#7b5e2a",
+	},
+	foodName: {
+		fontFamily: "Nunito_400Regular",
+		fontSize: 14,
+		color: "#000",
+		marginTop: 6,
+		textAlign: "center",
+	},
+	foodPrice: {
+		fontFamily: "Nunito_400Regular",
+		fontSize: 12,
+		color: "#B89A59",
+	},
 });
